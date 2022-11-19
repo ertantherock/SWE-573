7001f8ad059e88daea42a4812fdd568a732336bc
Ertan Kaya <ertancank@gmail.com>
2022-11-20 01:50:21 +0300

    Security upgrades with SpringSecurity

--- ws/ws/src/main/java/com/ePocket/ws/user/UserService.java
+++ ws/ws/src/main/java/com/ePocket/ws/user/UserService.java
@@ -1,6 +1,8 @@
 package com.ePocket.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service // Service layer for users. Layered arc.
@@ -8,8 +10,19 @@ public class UserService {
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	PasswordEncoder passwordEncoder;
+	
+	public UserService(UserRepository userRepository) {
+		this.userRepository=userRepository;
+		this.passwordEncoder= new BCryptPasswordEncoder();
+	}
+	
 
+	
 	public void save(User user) {
+		String encryptedPassword= this.passwordEncoder.encode(user.getPassword());
+		user.setPassword(encryptedPassword);
 		userRepository.save(user);
 		
 	}
