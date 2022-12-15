c708b6da499285db588bea5a7c7ca35ad88afd32
Ertan Kaya <ertancank@gmail.com>
2022-12-15 22:56:38 +0300

    Java project have been build with react project. .Jar file has been created. Dockerfile created and dockerized the whole project. Database has changed to Azure MySQL and application has been deployed

---
+++ ws/ws/src/main/resources/static/precache-manifest.134cf888ba8be3130000bc4a78ab109f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5ea09d2f1ce6513a559b7a196c98eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b29201e0e492b817ac",
+    "url": "/static/css/main.cbb0f232.chunk.css"
+  },
+  {
+    "revision": "e3a9f7396ab170473c4b",
+    "url": "/static/js/2.f8a919fa.chunk.js"
+  },
+  {
+    "revision": "dab5e9b0e1e253c82bf34a70b16bae69",
+    "url": "/static/js/2.f8a919fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b29201e0e492b817ac",
+    "url": "/static/js/main.e4db505a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e4db505a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  },
+  {
+    "revision": "486a8086d1376cb1dc142302d374af39",
+    "url": "/static/media/E.486a8086.png"
+  },
+  {
+    "revision": "be0bab6e696810611421bf88b27f38bc",
+    "url": "/static/media/hand.be0bab6e.svg"
+  },
+  {
+    "revision": "926fda932fcddf9dae79b8a3a560745b",
+    "url": "/static/media/profile.926fda93.png"
+  }
+]);
